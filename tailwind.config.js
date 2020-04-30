1ecaecfcfb008e6ac158d47527a60035b1de86ec
BjornLuG <bjornlu.dev@gmail.com>
2020-04-30 13:57:11 +0800

    Add Inter font

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ]
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
