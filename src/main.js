69d17d0db96125a8c48754240c42fb2c0ed96b99
BjornLuG <bjornlu.dev@gmail.com>
2020-04-30 14:18:27 +0800

    Add vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
 import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
