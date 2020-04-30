e0e17addfe3125afb79ce20e8e7b201ac695fef6
BjornLuG <bjornlu.dev@gmail.com>
2020-04-30 18:17:31 +0800

    Unpurge highlight css

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,8 @@ if (process.env.NODE_ENV === 'production') {
   plugins.push(
     require('@fullhuman/postcss-purgecss')({
       content: ['./public/**/*.html', './src/**/*.vue'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      whitelistPatterns: [/^hljs/]
     })
   )
 }
